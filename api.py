837ec14fb36548901f31bb93d6f565ba6ce532d1
Reno <longnguyenstocks@gmail.com>
2025-08-01 09:10:27 +0700

    Update api.py

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 def yeumoney(cookies, id, t='False'):
     if t == 'True':
-        time.sleep(15)
+        time.sleep(10)
     headers = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
     'accept-language': 'en-GB,en;q=0.7',
